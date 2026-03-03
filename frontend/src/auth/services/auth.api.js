7395f62bd17b77b7d012d4fc834ed8b483a45879
Aniket Lotlikar <alotlikar98@gmail.com>
2026-03-03 11:17:05 +0530

    api connection

---
+++ frontend/src/auth/services/auth.api.js
@@ -0,0 +1,61 @@
+import axios from "axios"
+
+export async function register({username, email,password}){
+
+    try {
+
+        const response =axios.post('http:://localhost:3000/api/auth/register', {
+            username, email,password
+        }, {
+            withCredentials: true
+        })
+
+        return response.data
+    }
+    catch (err) {
+        console.log(err)
+    }
+    
+}
+
+export async function login({ email, password }) {
+
+    try {
+
+        const response = await api.post("/api/auth/login", {
+            email, password
+        })
+
+        return response.data
+
+    } catch (err) {
+        console.log(err)
+    }
+
+}
+
+export async function logout() {
+    try {
+
+        const response = await api.get("/api/auth/logout")
+
+        return response.data
+
+    } catch (err) {
+
+    }
+}
+
+export async function getMe() {
+
+    try {
+
+        const response = await api.get("/api/auth/get-me")
+
+        return response.data
+
+    } catch (err) {
+        console.log(err)
+    }
+
+}
