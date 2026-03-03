44cb0fdf3f1eae84d7173b2c67d985796f6c2ad8
Aniket Lotlikar <alotlikar98@gmail.com>
2026-03-03 10:53:49 +0530

    login ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './app.routes'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
